bc2aefbc8228a545bcf91af8f543eee14af8a0ff
Naturekid <caibyyxf@sina.cn>
2016-01-14 16:17:56 +0800

    新的轨迹文件示例

--- src/TestDrive.java
+++ src/TestDrive.java
@@ -3,7 +3,7 @@ public class TestDrive {
 	
 	 public static void main(String[] args)
 	 {
-		 time_format tf = new time_format("e://Coding/Android/trace_management/gpscircle.txt");
+		 time_format tf = new time_format("e://Coding/Android/trace_management/gps.txt");
 		 tf.change_time_format();
 		 //this.change_time_format("./aodv.gps.xml");
 	 }
